afba3c1be16ba516031171602dba385230764c1f
Reyatsu99 <58881770+Reyatsu99@users.noreply.github.com>
2020-05-05 21:02:38 +0530

    Update nsfw.py

--- userbot/modules/nsfw.py
+++ userbot/modules/nsfw.py
@@ -1,12 +1,19 @@
-@bot.on(events.NewMessage(outgoing=True, pattern='.boobs'))
-@bot.on(events.MessageEdited(outgoing=True, pattern='.boobs'))
+import datetime
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.account import UpdateNotifySettingsRequest
+from userbot import bot, CMD_HELP
+from userbot.events import register
+
+@register(events.NewMessage(outgoing=True, pattern='.boobs'))
+@register(events.MessageEdited(outgoing=True, pattern='.boobs'))
 async def boobs(e):
     nsfw = requests.get('http://api.oboobs.ru/noise/1').json()[0]["preview"]
     urllib.request.urlretrieve("http://media.oboobs.ru/{}".format(nsfw), "*.jpg")
     os.rename('*.jpg', 'k.jpg')
     await bot.send_file(e.chat_id, "k.jpg")
-@bot.on(events.NewMessage(outgoing=True, pattern='.butts'))
-@bot.on(events.MessageEdited(outgoing=True, pattern='.butts'))
+@register(events.NewMessage(outgoing=True, pattern='.butts'))
+@register(events.MessageEdited(outgoing=True, pattern='.butts'))
 async def butts(e):
     nsfw = requests.get('http://api.obutts.ru/noise/1').json()[0]["preview"]
     urllib.request.urlretrieve("http://media.obutts.ru/{}".format(nsfw), "*.jpg")
