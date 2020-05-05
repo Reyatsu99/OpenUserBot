f7eaf0fdd58109cc7d6d65ab638b0baa3ea08d1c
Reyatsu99 <58881770+Reyatsu99@users.noreply.github.com>
2020-05-05 22:04:45 +0530

    Update nsfw.py

--- userbot/modules/nsfw.py
+++ userbot/modules/nsfw.py
@@ -5,14 +5,14 @@ from telethon.tl.functions.account import UpdateNotifySettingsRequest
 from userbot import bot, CMD_HELP
 from userbot.events import register
 
-register(outgoing=True, pattern="^.boobs(?: |$)(.*)")
+@register(outgoing=True, pattern="^.boobs(?: |$)(.*)")
 
 async def boobs(e):
     nsfw = requests.get('http://api.oboobs.ru/noise/1').json()[0]["preview"]
     urllib.request.urlretrieve("http://media.oboobs.ru/{}".format(nsfw), "*.jpg")
     os.rename('*.jpg', 'k.jpg')
     await bot.send_file(e.chat_id, "k.jpg")
-register(outgoing=True, pattern="^.butts(?: |$)(.*)")
+@register(outgoing=True, pattern="^.butts(?: |$)(.*)")
 
 async def butts(e):
     nsfw = requests.get('http://api.obutts.ru/noise/1').json()[0]["preview"]
