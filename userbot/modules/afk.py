4346cbbbe24e87559afd67fa99460aec58a91739
Mayur Karaniya <46866761+mkaraniya@users.noreply.github.com>
2020-05-18 22:11:45 +0530

    update authorship

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -1,6 +1,5 @@
 
 # Copyright (C) 2019 The Raphielscape Company LLC.
-#
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 # All Credits to https://t.me/azrim89 for timestamp.
